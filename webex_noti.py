8019a92fda2763041a88ba94bc5478c850752fa3
bruno barba <bruno.barba@gmail.com>
2021-02-08 15:41:09 -0600

    test

--- webex_noti.py
+++ webex_noti.py
@@ -5,7 +5,7 @@ apiUrl = 'https://webexapis.com/v1/messages'
 access_token = 'ZDc2ZDY0NjYtYzVlNC00MGY0LWI0NTItOThkZjRhZDIxZjFlYjViNWE1ZDctMDEw_P0A1_974dde6a-da6b-4f08-99e0-2e97dfbfbdac'
 room_ID = 'Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNWEwNzNjYzAtNmE0YS0xMWViLTllNjctNWYxYjVlZTU3Mjlh'
 
-httpHeaders = { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token }
+httpHeaders = { 'Content-type': 'application/json', 'Authorization': 'Bearer ' + access_token }
 
 body = { 'roomId': 'Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNWEwNzNjYzAtNmE0YS0xMWViLTllNjctNWYxYjVlZTU3Mjlh', 'text': 'Test notification 3 From Pyhton' }
 
