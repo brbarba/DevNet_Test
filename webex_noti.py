f3307d78feffc36c0ef033d0911aafa6ad01abf8
bruno barba <bruno.barba@gmail.com>
2021-02-08 15:39:06 -0600

    room id harcodeado

--- webex_noti.py
+++ webex_noti.py
@@ -7,7 +7,7 @@ room_ID = 'Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNWEwNzNjYzAtNmE0Y
 
 httpHeaders = { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token }
 
-body = { 'toPersonEmail': 'tofrench@webex.bot', 'text': 'Test notification From Pyhton' }
+body = { 'roomId': 'Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNWEwNzNjYzAtNmE0YS0xMWViLTllNjctNWYxYjVlZTU3Mjlh', 'text': 'Test notification 3 From Pyhton' }
 
 response = requests.post( url = apiUrl, json = body, headers = httpHeaders )
 
