7e064267d572f593d617544dcd0d6e180c66ffb2
bruno barba <bruno.barba@gmail.com>
2021-02-08 15:54:09 -0600

    changing the mail to be exactly like the guide

--- webex_noti.py
+++ webex_noti.py
@@ -5,7 +5,7 @@ import requests
 apiUrl = 'https://webexapis.com/v1/messages'
 access_token = 'ZDc2ZDY0NjYtYzVlNC00MGY0LWI0NTItOThkZjRhZDIxZjFlYjViNWE1ZDctMDEw_P0A1_974dde6a-da6b-4f08-99e0-2e97dfbfbdac'
 room_ID = 'Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNWEwNzNjYzAtNmE0YS0xMWViLTllNjctNWYxYjVlZTU3Mjlh'
-email_ID = 'bruno.barba@gmail.com'
+email_ID = 'gifbot@webex.bot'
 httpHeaders = { 'Content-type': 'application/json', 'Authorization': 'Bearer ' + access_token }
 
 #body = { 'roomId': room_ID, 'text': 'Test notification 4\nusando room_ID como variable\nFrom Pyhton' }
